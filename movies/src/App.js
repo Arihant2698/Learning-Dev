094bf0f87d1c045377525625544d6c7f3233d9d9
Arihant2698 <arihantjain2698@gmail.com>
2022-11-29 23:46:03 +0530

    Bugs fix

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
-import Movies from './Component/Movies'
+import Movies from './Components/Movies'
+import About from './Components/About';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 function App() {
   return (
-    <>
-    <Movies/>
-    </>
+    <BrowserRouter>
+    <Navbar/>
+    <Route path='/movies' component={Movies}/>
+    <Route path='/Home' component={Home}/>
+    <Route path='/About' component={About}/>
+    </BrowserRouter>
     );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//   <>
+  
+//   <BrowserRouter>
+//   <Navbar/>
+//   <Switch>
+//   <Route exact path='/' component={Home}/>
+//   <Route path='/movies' component={Movies}/>
+//   {/* <Route exact path='/about' component={About}/> */}
+//   <Route path='/about' render={(props)=>{ return <About {...props} isAuth={true} />}}/>
+//   </Switch>
+//   </BrowserRouter>
+//   </>
+//   );
+// }
+
+// export default App;
