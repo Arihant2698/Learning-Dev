aeb5e25924e2433e1b731703fe5da7e1136f9352
Arihant2698 <arihantjain2698@gmail.com>
2022-11-30 23:17:56 +0530

    Clean up

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -3,15 +3,16 @@ import Ue1 from "./useEffect/Ue1";
 import Ue2 from "./useEffect/Ue2";
 import Ue3 from "./useEffect/Ue3";
 import Infinite from "./useEffect/Infinite";
-
+import Uec1 from "./useEffect/UseEffect_Clean_up/Uec1";
 function App() {
   return (
     <>
     {/* <Uswo/> */}
      {/* <Ue1/> */}
     {/* <Ue2/> */}
-    <Ue3/>
+    {/* <Ue3/> */}
     {/* <Infinite/> */}
+    <Uec1/>
     </>
   );
 }
