52be025f46c9b020fa0551dfa275b33f99ba7a3d
Arihant2698 <arihantjain2698@gmail.com>
2022-12-01 21:40:54 +0530

    Comman Syntax

---
+++ firebase/src/Components/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+
+
+firebase.initializeApp(
+{
+apiKey: "AIzaSyC6yIW3abVDd18rLv_dIHgpjM_l1dZUVd0",
+  authDomain: "fir-project-2fe66.firebaseapp.com",
+  projectId: "fir-project-2fe66",
+  storageBucket: "fir-project-2fe66.appspot.com",
+  messagingSenderId: "932285463227",
+  appId: "1:932285463227:web:4fdde48d03ec11df56879e"
+})
+export default firebase;
