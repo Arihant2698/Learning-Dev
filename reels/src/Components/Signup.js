d9fd511ee023d154e2568393f80181228a7a55ff
Arihant2698 <arihantjain2698@gmail.com>
2022-12-05 01:19:43 +0530

    added file_upload feature in signup

--- reels/src/Components/Signup.js
+++ reels/src/Components/Signup.js
@@ -1,37 +1,96 @@
-import React,{useState,useEffect,useContext} from 'react';
+import React,{useState,useEffect,useContext} from 'react'
 import {AuthContext} from '../Context/AuthProvider';
-export default function Signup() {
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const [name,setName] = useState('');
-  const [error,setError]= useState('');
-  const [loading,setLoading]=useState(false);
-  const {signup} = useContext(AuthContext);
-    const handleSignUp=async (e)=>{
+import { storage,database } from '../firebase';
+function Signup() {
+    const [email,setEmail] =useState('');
+    const[password,setPassword] = useState('');
+    const [name,setName] =useState('');
+    const[error,setError] = useState('');
+    const[loading,setLoading] = useState(false);
+    const [file,setFile] = useState(null)
+    const {signup} =useContext(AuthContext);
+    console.log(signup);
+    const handleSignup =async (e)=>{
         e.preventDefault();
+        try{
         setLoading(true);
-        let res =await signup(email,password);
+        let res = await signup(email,password);
         let uid = res.user.uid;
-        setLoading(false);
+        console.log(uid); 
+        const uploadTaskListener = storage.ref(`/users/${uid}/profileImage`).put(file);    //In Storage if it is not working than we have to edit the permission in firebase/storage
+        // Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+        // fn 1 -> progress tracking
+        // fn2 -> error
+        // fn3 -> success
+        uploadTaskListener.on('state_changed',fn1,fn2,fn3);
+        function fn1(snapshot){
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');         
+        }
+        function fn2(error){
+            setError(error);
+            setTimeout(()=>{
+                setError('')
+            },2000);
+            setLoading(false)
+        }
+        async function fn3(){
+            let downloadUrl = await uploadTaskListener.snapshot.ref.getDownloadURL();
+            console.log(downloadUrl);
+          await database.users.doc(uid).set({
+                email:email,
+                userId:uid,
+                username:name,
+                createdAt:database.getCurrentTimeStamp(),
+                profileUrl:downloadUrl,
+                postIds:[]
+            })
+            setLoading(false);
+            console.log('User has Signed up');
+        }
+        
+    }
+    catch(err){
+        setError(err)
+        setTimeout(()=>setError(''),2000);
+        setLoading(false)
+    }
+    }
+    const handleFileSubmit=(e)=>{
+        let file = e.target.files[0];
+        console.log(file);
+        if(file!=null)
+        {
+            setFile(file)
+        }
     }
     return (
-    <div>
-        <form onSubmit={handleSignUp} >
-            <div>
-                <label htmlFor=''>UserName</label>
-                <input type='text' value={name} onChange={(e)=>{setName(e.target.value)}}/>
-            </div>
-            <div>
-                <label htmlFor=''>Email</label>
-                <input type='email' value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-            </div>
-            <div>
-                <label htmlFor=''>Password</label>
-                <input type='password' value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-            </div>
-            <button type='submit' disabled={loading}>Login</button>
-        </form>
+        <div>
+            <form onSubmit={handleSignup} >
+                <div>
+                    <label htmlFor=''>UserName</label>
+                    <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
 
-    </div>
-  )
+                </div>
+                <div>
+                <label htmlFor=''>Email</label>
+                    <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                </div>
+                <div>
+                <label htmlFor=''>Password</label>
+                    <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor='profile'>Profile image</label>
+                    <input type='file' accept='image/*' onChange={handleFileSubmit}></input>
+                </div>
+                <button type='submit' disabled={loading}>Login</button>
+            </form>
+        </div>
+    )
 }
+
+export default Signup
