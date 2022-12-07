26d4dff7edf3e01f20cf9b0aa7ddc9f7a0b078dd
Arihant2698 <arihantjain2698@gmail.com>
2022-12-07 19:13:13 +0530

    added header and upload btn in feed page

--- reels/src/Components/Feed.js
+++ reels/src/Components/Feed.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React,{useState,useEffect,useContext} from 'react'
+import Header from './Header';
+import { AuthContext } from '../Context/AuthProvider'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import UploadFile from './UploadFile';
+import './Feed.css'
+import { database } from '../firebase.js';
 export default function Feed() {
-  return (
-    <div>
-        <h1>Okkk</h1>
-    </div>
-  )
+    const {currentUser} =useContext(AuthContext);
+    const [userData,setUserData] = useState(null);
+    useEffect(()=>{
+        const unsub = database.users.doc(currentUser.uid).onSnapshot((doc)=>{
+            // console.log(doc.data());
+            setUserData(doc.data())
+        })
+    },[currentUser])
+    return (
+        <>
+        { userData==null ? <CircularProgress />:<>
+        <Header userData={userData} />
+        <div style={{height:'9.5vh'}}/>
+        <div className='feed-container'>
+            <div className='center'>
+                <UploadFile userData={userData}/>
+            </div>
+        </div>
+
+        </>
+        }
+        </>
+    )
 }
