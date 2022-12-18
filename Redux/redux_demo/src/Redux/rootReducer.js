7cd82af9798da637935482519545b757eddaae2a
Arihant2698 <arihantjain2698@gmail.com>
2022-12-18 18:48:32 +0530

    remove

--- Redux/redux_demo/src/Redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import BallReducer from "./balls/ballReducer";
-import BatReducer from "./bats/batReducer";
-const rootReducer = combineReducers({
-    ball:BallReducer,
-    bat:BatReducer
-})
-export default rootReducer;
